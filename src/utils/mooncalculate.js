9fc912fc6edbe7f5e715d8c33130dae6f10f5f62
Azizjon <aliqulovazizjon68@gmail.com>
2023-09-12 21:24:25 +0500

    updated

--- src/utils/mooncalculate.js
+++ src/utils/mooncalculate.js
@@ -6,8 +6,15 @@ function moon(date) {
   const lng = 69.2947;
   let moonPhase = SunCalc.getMoonIllumination(time, lat, lng).phase;
   moonPhase = Math.floor((moonPhase * 100) / 3.2);
+  // console.log(moonPhase);
   let url = `https://t.me/moon_aziz/${33 - moonPhase}`;
-  const link = path.join(__dirname, `../public/${33 - moonPhase}.jpg`);
+  const link = path.join(__dirname, `../public/${32 - moonPhase}.jpg`);
+  // if (moonPhase == 0) console.log(url, " ", link, " ", moonPhase);
+  // if (moonPhase == 31) console.log(link, "url", url);
   return { url, link };
 }
+
+// for (let i = 1; i <= 2023; i++) {
+// moon(`01-01-${2023 - i}`);
+// }
 module.exports = moon;
